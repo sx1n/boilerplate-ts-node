b563f157272d8596622bfdf0dac7536740fa31b7
sx1n <samuellaurindodelima2004@gmail.com>
2023-03-18 07:26:38 -0300

    chore: add jest setup and config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+  coverageProvider: "v8",
+};
